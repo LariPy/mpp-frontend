2535d362f5655b3ea3b69939aa66b5f71f5dd237
Pyykkö Lari 20955 <lari.pyykko@edu.riveria.fi>
2023-01-27 11:39:54 +0200

    first commit

---
+++ src/components/Public.js
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom'
+
+const Public = () => {
+    const content = (
+        <section>
+            <header>
+
+            </header>
+            <main>
+
+            </main>
+            <footer>
+
+            </footer>
+        </section>
+    )
+  
+    return content
+}
+
+export default Public
+
+// need to add classnames and do css stuff
+// also some content
